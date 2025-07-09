555d11dbfbd1dca7231f196dd82751f765fcee5d
mohamed ali ferchichi <148057675+Ferdali10@users.noreply.github.com>
2025-07-09 14:35:55 +0100

    Update buildProject.groovy

--- vars/buildProject.groovy
+++ vars/buildProject.groovy
@@ -1,8 +1,7 @@
-def call(Map config = [:]) {
-    def buildTool = config.buildTool ?: 'maven'
-
+def call(String buildTool, Map config = [:]) {
     stage('🏗 Build Spring Boot + MySQL') {
         script {
+            // Vérification du fichier de config DB
             def dbConfigFile = 'src/main/resources/application.properties'
             if (!fileExists(dbConfigFile)) {
                 error "Fichier de configuration DB introuvable: ${dbConfigFile}"
@@ -10,28 +9,35 @@ def call(Map config = [:]) {
 
             switch(buildTool.toLowerCase()) {
                 case 'maven':
+                    // Construction de la commande Maven
                     def mvnCmd = "./mvnw clean package"
 
-                    if (config.activeProfile) {
+                    // Ajout d'arguments personnalisés (ex: -Pprod, profils, etc)
+                    if (config.args) {
+                        mvnCmd += " ${config.args}"
+                    } else if (config.activeProfile) {
                         mvnCmd += " -Dspring.profiles.active=${config.activeProfile}"
                     } else {
                         echo "[INFO] Utilisation de la configuration par défaut (sans profil spécifique)"
                     }
 
+                    // Gestion des tests
                     if (config.skipDbTests) {
-                        mvnCmd += " -Dtest=!*RepositoryTest,*ServiceTest"
+                        mvnCmd += " -Dtest=!*RepositoryTest,*ServiceTest" // Exclut certains tests
                     } else if (config.skipAllTests) {
                         mvnCmd += " -DskipTests"
                     }
 
-                    if (config.args) {
-                        mvnCmd += " ${config.args}"
-                    }
+                    // S'assurer que ./mvnw est exécutable
+                    sh 'chmod +x ./mvnw'
 
+                    // Exécution de la commande
                     sh mvnCmd
 
+                    // Archive des fichiers jar générés
                     archiveArtifacts artifacts: 'target/*.jar,target/libs/*.jar', fingerprint: true
 
+                    // Vérification qu'un JAR a bien été généré
                     if (!fileExists('target/*.jar')) {
                         error "Erreur : Aucun JAR généré. Vérifiez les logs Maven."
                     }
@@ -48,4 +54,5 @@ def call(Map config = [:]) {
         }
     }
 }
+
 
