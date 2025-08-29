23ddb8eb786ce404c723bf923f7073f4b46e1852
mohamed ali ferchichi <148057675+Ferdali10@users.noreply.github.com>
2025-08-29 14:43:21 +0100

    Update kubernetesDeploy.groovy

--- vars/kubernetesDeploy.groovy
+++ vars/kubernetesDeploy.groovy
@@ -1,20 +1,18 @@
 def call(Map params = [:]) {
     String kubeConfigPath = params.kubeConfigPath ?: '/var/lib/jenkins/.kube/config'
     String manifestDir = params.manifestDir ?: 'k8s'
-
-    stage('🚀 Deploy to Kubernetes') {
-        script {
-            if (!fileExists(manifestDir)) {
-                error "❌ Dossier des manifests Kubernetes introuvable: ${manifestDir}"
-            }
-
-            withEnv(["KUBECONFIG=${kubeConfigPath}"]) {
-                sh """
-                    echo "📂 Déploiement depuis: ${manifestDir}"
-                    kubectl apply -f ${manifestDir}/
-                    kubectl rollout status deployment/springfoyer-deployment --timeout=120s
-                """
-            }
+    
+    script {
+        if (!fileExists(manifestDir)) {
+            error "❌ Dossier des manifests Kubernetes introuvable: ${manifestDir}"
+        }
+        
+        withEnv(["KUBECONFIG=${kubeConfigPath}"]) {
+            sh """
+                echo "📂 Déploiement depuis: ${manifestDir}"
+                kubectl apply -f ${manifestDir}/
+                kubectl rollout status deployment/springfoyer-deployment --timeout=120s
+            """
         }
     }
 }
